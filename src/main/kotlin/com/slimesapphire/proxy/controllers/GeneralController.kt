02e966864042a8100cb6a0067ba1ccdf9e485351
Vitalik Hashchuk <vitalikhashcuk@gmail.com>
2018-11-07 12:50:35 +0200

    Add basic index page template

--- src/main/kotlin/com/slimesapphire/proxy/controllers/GeneralController.kt
+++ src/main/kotlin/com/slimesapphire/proxy/controllers/GeneralController.kt
@@ -2,12 +2,19 @@ package com.slimesapphire.proxy.controllers
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.ModelAndView
+import java.util.HashMap
+
+
 
 @Controller
 class GeneralController {
 
     @GetMapping("/")
-    fun index(): String {
-        return "index"
+    fun index(): ModelAndView {
+        val model = HashMap<String, String>()
+        model.put("name", "your name")
+
+        return ModelAndView("index", model)
     }
 }
